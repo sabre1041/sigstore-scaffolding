12a05da01de3b0a21d2ee51354db9ef64dee6d3f
Andrew Block <andy.block@gmail.com>
2024-04-13 04:03:35 -0500

    Enhancements to support tuf updates
    Signed-off-by: Andrew Block <andy.block@gmail.com>

--- pkg/repo/repo.go
+++ pkg/repo/repo.go
@@ -30,6 +30,7 @@ import (
 	"time"
 
 	"github.com/theupdateframework/go-tuf"
+	"github.com/theupdateframework/go-tuf/data"
 	"knative.dev/pkg/logging"
 )
 
@@ -72,7 +73,7 @@ func CreateRepoWithMetadata(ctx context.Context, targets []TargetWithMetadata) (
 		return nil, "", fmt.Errorf("failed to NewRepoIndent: %w", err)
 	}
 
-	if err := r.Init(false); err != nil {
+	if err := r.Init(true); err != nil {
 		return nil, "", fmt.Errorf("failed to Init repo: %w", err)
 	}
 
@@ -80,9 +81,9 @@ func CreateRepoWithMetadata(ctx context.Context, targets []TargetWithMetadata) (
 	expires := time.Now().AddDate(0, 6, 0)
 
 	for _, role := range []string{"root", "targets", "snapshot", "timestamp"} {
-		_, err := r.GenKeyWithExpires(role, expires)
+		_, err := r.GenKeyWithSchemeAndExpires(role, expires, data.KeySchemeECDSA_SHA2_P256)
 		if err != nil {
-			return nil, "", fmt.Errorf("failed to GenKeyWithExpires: %w", err)
+			return nil, "", fmt.Errorf("failed to GenKeyWithSchemeAndExpires: %w", err)
 		}
 	}
 
